d0ef6a974596d8b23242415e84693ab885933cce
Merih Bora Pocan <mborapocan@gmail.com>
2023-12-30 01:46:37 +0300

    Signals and blocks are updated

---
+++ config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DELIMITER " "
+
+#define MAX_BLOCK_OUTPUT_LENGTH 45
+
+#define CLICKABLE_BLOCKS 1
+
+#define LEADING_DELIMITER 0
+
+#define TRAILING_DELIMITER 0
+
+#define BLOCKS(X)         \
+    X("sb-kbselect", 0, 5)     \
+    X("sb-bluetooth", 1, 2)  \
+    X("sb-internet", 1, 1) \
+    X("sb-volume", 1, 3)  \
+    X("sb-battery", 1, 4) \
+    X("sb-clock", 1, 6) \
+
+#endif  // CONFIG_H
