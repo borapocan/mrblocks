bbf840239a8186f3d7851e17799fa5739352f319
Merih Bora Pocan <mborapocan@gmail.com>
2023-12-31 22:29:40 +0300

    Brightness script and block added

--- config.h
+++ config.h
@@ -12,11 +12,11 @@
 #define TRAILING_DELIMITER 0
 
 #define BLOCKS(X)         \
-    X("sb-kbselect", 0, 5)     \
+    X("sb-kbselect", 10, 5)     \
+    X("sb-brightness", 1, 6)     \
     X("sb-bluetooth", 1, 2)  \
     X("sb-internet", 1, 1) \
     X("sb-volume", 1, 3)  \
     X("sb-battery", 1, 4) \
-    X("sb-clock", 1, 6) \
 
 #endif  // CONFIG_H
